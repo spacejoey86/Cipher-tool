c1950146031b9390904f8eac8336bc9a2369a25f
Dylan Gardham <message4dylan@gmail.com>
2021-08-31 19:32:51 +0100

    Added classes for decoding stages

--- Stages.py
+++ Stages.py
@@ -1,5 +1,24 @@
 class Stage:
     name = "stage name"
     button = None
+
 class Input(Stage):
     name = "Input"
+
+class Capitalise(Stage):
+    name = "Capitalise"
+class RemoveSpaces(Stage):
+    name = "Remove Spaces"
+class Reverse(Stage):
+    name = "Reverse"
+
+class CaesarShift(Stage):
+    name = "Caesar shift"
+class Substitution(Stage):
+    name = "Substitution"
+class Affine(Stage):
+    name = "Affine"
+class Viginere(Stage):
+    name = "Viginere"
+class Transposition(Stage):
+    name = "Transposition"
