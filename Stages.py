b35380751ee0128e48691d83252601a988aa441e
Dylan Gardham <message4dylan@gmail.com>
2021-08-31 21:17:46 +0100

    completed update logic

--- Stages.py
+++ Stages.py
@@ -1,12 +1,41 @@
+import tkinter as tk
+
 class Stage:
     name = "stage name"
     button = None
+    update_function = None
+    def __init__(self, frame, updateFunction):
+        update_function = updateFunction
+    def process(self, text):
+        return text
+    def display(self):
+        pass
 
 class Input(Stage):
     name = "Input"
-
+    textbox = None
+    def onModify(self, blah):
+        try:
+            self.textbox.tk.call(self.textbox._w, 'edit', 'modified', 0)
+        finally:
+            self.updateFunction()
+    def __init__(self, frame, updateFunction):
+        self.updateFunction = updateFunction
+        self.textbox = tk.Text(frame)
+        self.textbox.bind('<<Modified>>',self.onModify)
+    def display(self):
+        self.textbox.grid()
+    def process(self, text):
+        return self.textbox.get("1.0",tk.END)
+class Output(Stage):
+    name = "Output"
+    def process(self, text):
+        print(text)
+    
 class Capitalise(Stage):
     name = "Capitalise"
+    def process(self, text):
+        return text.upper()
 class RemoveSpaces(Stage):
     name = "Remove Spaces"
 class Reverse(Stage):
--- ui.py
+++ ui.py
@@ -4,6 +4,43 @@ from Stages import *
 root = tk.Tk()
 root.title("Cipher program")
 
+def updateStageEditor():
+    for child in stage_editor.winfo_children():
+        child.grid_forget()
+    stages[selected_stage.get()].display()
+    root.focus_set()
+stage_editor = tk.Frame(root)
+stage_editor.grid(row=0, column=0)
+
+stages = []
+def addStage(stage):
+    stages.append(stage)
+    updateStagesFrame()
+    stages[len(stages)-1].button.select()
+    updateStageEditor()
+    updateOutputText()
+selected_stage = tk.IntVar()
+stages_frame = tk.Frame(root)
+stages_frame.grid(row=0, column=1, sticky="NS")
+def updateStagesFrame():
+    for button in stages_frame.winfo_children():
+        button.destroy()
+    for stage_index in range(len(stages)):
+        stage = stages[stage_index]
+        stage.button = tk.Radiobutton(stages_frame, text=stage.name, variable = selected_stage, value = stage_index, command=updateStageEditor,
+                                      indicatoron = 0, width = 20)
+        stage.button.grid()
+updateStagesFrame()
+
+def updateOutputText():
+    text = ""
+    for stage in stages:
+        text = stage.process(text)
+    right_text.delete(1.0, tk.END)
+    right_text.insert(tk.END,text)
+right_text = tk.Text(root)
+right_text.grid(row=0, column=2)
+
 menu = tk.Menu(root)
 file_menu = tk.Menu(menu, tearoff=0)
 file_menu.add_command(label="Open")
@@ -18,37 +55,17 @@ ana_menu.add_checkbutton(label="Index of Coincidence")
 ana_menu.add_checkbutton(label="Bigrams")
 menu.add_cascade(label="Analyse", menu=ana_menu)
 text_menu = tk.Menu(menu, tearoff=0)
-text_menu.add_command(label="Capitalise", command=lambda:[stages.append(Capitalise()),updateStagesFrame()])
-text_menu.add_command(label="Remove spaces", command=lambda:[stages.append(RemoveSpaces()),updateStagesFrame()])
-text_menu.add_command(label="Reverse", command=lambda:[stages.append(Reverse()),updateStagesFrame()])
+text_menu.add_command(label="Capitalise", command=lambda:addStage(Capitalise(stage_editor, updateOutputText)))
+text_menu.add_command(label="Remove spaces", command=lambda:addStage(RemoveSpaces(stage_editor, updateOutputText)))
+text_menu.add_command(label="Reverse", command=lambda:addStage(Reverse(stage_editor, updateOutputText)))
 menu.add_cascade(label="Text stage", menu=text_menu)
 solve_menu = tk.Menu(menu, tearoff=0)
-solve_menu.add_command(label="Caesar Shift", command=lambda:[stages.append(CaesarShift()),updateStagesFrame()])
-solve_menu.add_command(label="Substitution", command=lambda:[stages.append(Substitution()),updateStagesFrame()])
-solve_menu.add_command(label="Affine", command=lambda:[stages.append(Affine()),updateStagesFrame()])
-solve_menu.add_command(label="Viginere", command=lambda:[stages.append(Viginere()),updateStagesFrame()])
-solve_menu.add_command(label="Transposition", command=lambda:[stages.append(Transposition()),updateStagesFrame()])
+solve_menu.add_command(label="Caesar Shift", command=lambda:addStage(CaesarShift(stage_editor, updateOutputText)))
+solve_menu.add_command(label="Substitution", command=lambda:addStage(Substitution(stage_editor, updateOutputText)))
+solve_menu.add_command(label="Affine", command=lambda:addStage(Affine(stage_editor, updateOutputText)))
+solve_menu.add_command(label="Viginere", command=lambda:addStage(Viginere(stage_editor, updateOutputText)))
+solve_menu.add_command(label="Transposition", command=lambda:addStage(Transposition(stage_editor, updateOutputText)))
 menu.add_cascade(label="Solve stage", menu=solve_menu)
 root.config(menu=menu)
 
-stage_editor = tk.Frame(root)
-stage_editor.grid(row=0, column=0)
-
-#stages list
-stages = [Input()]
-selected_stage = tk.IntVar()
-stages_frame = tk.Frame(root)
-stages_frame.grid(row=0, column=1, sticky="NS")
-def updateStagesFrame():
-    for button in stages_frame.winfo_children():
-      button.destroy()
-    for stage_index in range(len(stages)):
-        stage = stages[stage_index]
-        stage.button = tk.Radiobutton(stages_frame, text=stage.name, variable = selected_stage, value = stage_index,
-                                      indicatoron = 0, width = 20)
-        stage.button.grid()
-updateStagesFrame()
-
-
-right_text = tk.Text(root)
-right_text.grid(row=0, column=2)
+addStage(Input(stage_editor, updateOutputText))
