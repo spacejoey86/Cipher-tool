3dfbeeb7ecdea00a1db9b77c001f8b09ba008e6c
spacejoey86 <message4dylan@gmail.com>
2021-10-26 16:13:41 +0100

    added analysis stage WordFinder for monoalphabetic substitutions

--- Analysis_stages.py
+++ Analysis_stages.py
@@ -34,6 +34,64 @@ class PlayfairDetect(Stage):
         else:
             self.output.configure(text="Doubles not found, this could be a Playfair cipher")
         return text
+class WordFinder(Stage):
+    name = "Word Finder"
+    output = None
+    def __init__(self, frame, updateFunction):
+        self.frame = frame
+        self.updateFunction = updateFunction
+        self.input_var = tk.StringVar()
+        self.input = tk.Entry(frame, width=20, textvariable=self.input_var)
+        self.input.configure(font=('Courier New', 10)) #same font so it lines up with the output
+        self.input_var.trace_add("write", lambda a, b, c, self=self : self.updateFunction())
+        self.output = tk.Label(frame, text="")
+        self.output.configure(font=('Courier New', 10))
+    def display(self):
+        self.input.grid(sticky="NW", padx=2) #padding so it lines up with the output
+        self.output.grid(sticky="NW")
+        self.frame.rowconfigure(0, weight=0)
+        self.frame.rowconfigure(1, weight=0)
+        self.frame.columnconfigure(0, weight=0)
+        self.frame.columnconfigure(1, weight=0)
+    def changeformat(self, text): #returns an identifier representing where there are repeated letters in the string (but not what letters they are)
+        nextLetter = 0
+        outputText = []
+        for index, letter in enumerate(text):
+            if outputText == []:
+                outputText.append(Constants.alphabet[nextLetter])
+                nextLetter += 1
+            else:
+                repeated = -1
+                for i, v in enumerate(range(0, index)):
+                    if letter == text[i]:
+                        repeated = i
+                if repeated == -1:
+                    outputText.append(Constants.alphabet[nextLetter])
+                    nextLetter += 1
+                else:
+                    outputText.append(outputText[repeated])
+        return outputText
+    def process(self, text):
+        #self.input needs to have repeated letters, else it will match everything
+        matches = {}
+        for index, letter in enumerate(text):
+            testWord = ""
+            if len(text) - len(self.input.get()) >= index:
+                for v in range(len(self.input.get())):
+                    testWord += text[index + v]
+            else:
+                break
+            if self.changeformat(self.input.get().lower()) == self.changeformat(testWord):
+                if not testWord in matches:
+                    matches[testWord] = 0
+                matches[testWord] += 1
+        output = ""
+        for match in sorted(matches, key=matches.get, reverse=True):
+            output += match + " : " + str(matches[match]) + "\n"
+        if len(self.input.get()) == 0:
+            output = ""
+        self.output.configure(text=output)
+        return text
 class FrequencyAnalyse(Stage):
     name = "Frequency analysis"
     output = None
